69878b10553ca0679cf25d8e07c566780a49654d
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-06-27 21:33:02 +0400

    no commit yet.

--- Account/views.py
+++ Account/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
 
 def login_request(request):
 	return render(request,"Account/login.html")
 def register_request(request):
 	return render(request,"Account/login.html")
 def logout_request(request):
+	logout(request)
 	return redirect("home")
