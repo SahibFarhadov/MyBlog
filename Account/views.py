c971da7fd6092cd4d2fc7e2682e545c0df6c2e0a
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-06-28 23:07:51 +0400

    Login sehifesi yigildi. Istifadeci login ola bilir.

--- Account/views.py
+++ Account/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import logout
+from django.contrib.auth import login,logout,authenticate
 
 def login_request(request):
+	if request.method=="POST":
+		username = request.POST["username"]
+		password = request.POST["password"]
+
+		user=authenticate(request,username=username,password=password)
+		if user is not None:
+			login(request,user)
+			return redirect("home")
+		else:
+			return render(request,"Account/login.html",{
+				"error":"username ve parol yanlisdir"
+				})
+
 	return render(request,"Account/login.html")
 def register_request(request):
-	return render(request,"Account/login.html")
+	return render(request,"Account/register.html")
 def logout_request(request):
 	logout(request)
 	return redirect("home")
