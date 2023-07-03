dfd9ac327239946935a6e56dc1e484b7b4fd728f
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-07-03 04:56:08 +0400

    Resgister sehifesi islenildi. Qeydiyyatdan kece bilirik :=)

--- Account/views.py
+++ Account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import login,logout,authenticate
+from django.contrib.auth.models import User
 
 #account app ucun login metodu
 def login_request(request):
@@ -23,6 +24,37 @@ def login_request(request):
 def register_request(request):
 	if request.user.is_authenticated:
 		return redirect('home')
+	if request.method=="POST":
+		# qeydiyyat formundan melumatlarin alinmasi
+		name=request.POST["name"]
+		surname=request.POST["surname"]
+		nickname=request.POST["nickname"]
+		email=request.POST["email"]
+		password=request.POST["password"]
+		repassword=request.POST["repassword"]
+
+		errorMessage=""
+		dataKeep={
+			"name":name,
+			"surname":surname,
+			"nickname":nickname,
+			"email":email,
+			"errorMessage":errorMessage
+		}
+		if password==repassword:
+			if User.objects.filter(username=nickname).exists():
+				dataKeep["errorMessage"]="Daxil edilən istifadəçi adı artıq mövcuddur"
+				return render(request, "Account/register.html", dataKeep )
+			elif User.objects.filter(email=email).exists():
+				dataKeep["errorMessage"]="Daxil edilən email ünvanı artıq istifadə edilmişdir"
+				return render(request, "Account/register.html", dataKeep )
+			else:
+				User.objects.create_user(username=nickname,first_name=name,last_name=surname,password=password,email=email)
+				return redirect('login')
+			
+
+
+
 	return render(request,"Account/register.html")
 
 #account app ucun logout metodu
