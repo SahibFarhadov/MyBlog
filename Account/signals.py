d1dd0ba882c79d4823e8414d70b42486fb31cd57
Sahib Fərhadov <sahibcebrayilli@gmail.com>
2023-07-03 06:12:52 +0400

    signals.py silindi

--- Account/signals.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import login,logout,authenticate
-from django.contrib.auth.models import User
-from django.dispatch import receiver
-from django.db.models.signals import post_save
-
-@receiver(post_save,sender=User)
-def login_user(sender, instance, created, **kwargs):
-    if created:
-        login(instance)
